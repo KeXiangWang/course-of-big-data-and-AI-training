e7535f91fa5f2cbfa819e9e25145d4ea8d053e66
王科翔 <kx.wang@hotmail.com>
2019-04-23 12:32:35 +0800

    add SVM in 8th

---
+++ 8th/SVM.py
@@ -0,0 +1,24 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+
+def SVM(acc_sequence, gyr_sequence, label_sequence):
+    x = np.append(gyr_sequence, acc_sequence, axis=2)
+    print(x.shape)
+    y = label_sequence
+    x_train, x_verify, y_train, y_verify = train_test_split(x, y, train_size=0.9, test_size=0.1, random_state=1)
+    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, train_size=0.88, test_size=0.12,
+                                                        random_state=1)
+    model = svm.SVC(gamma="scale")
+    y_test_res = np.ones([6, len(y_test)])
+    print("shape of test:", y_test_res.shape)
+    for i in range(6):
+        model.fit(x_train[:, :, i], y_train)
+        y_test_res[i] = model.predict(x_test[:, :, i])
+        print('accuracy_score[{}]:'.format(i), accuracy_score(y_test, y_test_res[i]))
+    y_test_hat = np.ones(len(y_test))
+    for i in range(len(y_test)):
+        y_test_hat[i] = np.argmax(np.bincount(y_test_res[:, i].astype(np.int16)))
+    print('accuracy_score:', accuracy_score(y_test, y_test_hat))
--- 8th/__main__.py
+++ 8th/__main__.py
@@ -1,11 +1,10 @@
 import data_process
 from tree import *
+from SVM import *
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
 
 if __name__ == "__main__":
     acc_sequence, gyr_sequence, label_sequence = data_process.get_sequence()
     # tree(acc_sequence, gyr_sequence, label_sequence)
+    # SVM(acc_sequence, gyr_sequence, label_sequence)
     print("Hello World!")
