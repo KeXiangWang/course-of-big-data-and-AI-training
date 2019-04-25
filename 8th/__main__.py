2a0c210e31d06341915fa044ee60df6edb024dda
王科翔 <kx.wang@hotmail.com>
2019-04-25 18:24:00 +0800

    finish 8th

--- 8th/__main__.py
+++ 8th/__main__.py
@@ -1,10 +1,12 @@
 import data_process
 from tree import *
 from SVM import *
+from CNN import *
 import numpy as np
 
 if __name__ == "__main__":
     acc_sequence, gyr_sequence, label_sequence = data_process.get_sequence()
     # tree(acc_sequence, gyr_sequence, label_sequence)
-    # SVM(acc_sequence, gyr_sequence, label_sequence)
+    SVM(acc_sequence, gyr_sequence, label_sequence)
+    # CNN(acc_sequence, gyr_sequence, label_sequence)
     print("Hello World!")
